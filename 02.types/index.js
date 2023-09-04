6af9e6d2b282eb29d9dfeb4747e37d4e8d429528
Thaina Carvalho <thaina.carvalho@driven.cx>
2023-09-04 10:44:18 -0300

    scalar types

--- 02.types/index.js
+++ 02.types/index.js
@@ -1,10 +1,43 @@
 const { gql, ApolloServer } = require("apollo-server")
 
-const typeDefs = gql`
+/*
+  * Scalar Types
+    - Int
+    - Float
+    - String
+    - Boolean
+    - ID
+*/
 
+const typeDefs = gql`
+  type Query {
+    idade: Int
+    salario: Float
+    nome: String
+    ativo: Boolean
+    id: ID
+  }
 `
 
-const resolvers = {}
+const resolvers = {
+  Query: {
+    idade () {
+      return 18
+    },
+    salario () {
+      return 188.155
+    },
+    nome () {
+      return 'Graphql'
+    },
+    ativo () {
+      return true
+    },
+    id () {
+      return 1554597843106
+    }
+  }
+}
 
 
 const server = new ApolloServer({
