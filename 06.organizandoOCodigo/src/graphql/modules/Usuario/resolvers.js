aceaf31079dde56fda875af2ce8fef961df352de
Thaina Carvalho <thaina.carvalho@driven.cx>
2023-10-02 11:34:33 -0300

    mutation criar usuario

--- 06.organizandoOCodigo/src/graphql/modules/Usuario/resolvers.js
+++ 06.organizandoOCodigo/src/graphql/modules/Usuario/resolvers.js
@@ -1,4 +1,17 @@
 const db = require('../../../db')
+
+function geradorDeId(lista) {
+  let novoId;
+  let ultimoId = lista[lista.length -1]
+
+  if(!ultimoId) {
+    novoId = 0
+  } else {
+    novoId = ultimoId.id;
+  }
+  return ++novoId
+}
+
 module.exports = {
   Usuario: {
     perfil(usuario) {
@@ -11,5 +24,17 @@ module.exports = {
       return db.usuarios.find((db) => db.id === args.id);
     },
     usuarios: () => db.usuarios
+  },
+
+  Mutation: {
+    criarUsuario(_, args) {
+      const novoUsuario = {
+        ...args,
+        id: geradorDeId(db.usuarios),
+        perfil: 2
+      }
+      db.usuarios.push(novoUsuario)
+      return novoUsuario
+    }
   }
 }
